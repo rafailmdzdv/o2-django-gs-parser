1547ea9076cfef5bfa8381069be841bea714a819
rafailka-m <rafail.medzhidov@yandex.ru>
2023-03-21 21:41:04 +0300

    feat(gs list): download gas stations list xls file from rss.tatneft.ru

--- gs_parser/app/services/pw.py
+++ gs_parser/app/services/pw.py
@@ -1,19 +1,21 @@
 from playwright.sync_api import Browser, Page, sync_playwright
 
-from gs_parser.settings import CLICKING_DELAY
+from gs_parser import settings
 
 
 def start_interacting():
     with sync_playwright() as sp:
         browser = sp.chromium.launch(headless=False)
-        page = _open_rss_page(browser)
-        _start_clicking_buttons(page)
+        main_page = _open_rss_page(browser)
+        _start_clicking_buttons(main_page)
+        _get_gas_stations_xls(browser)
         browser.close()
 
 
-def _open_rss_page(browser: Browser) -> Page:
+def _open_rss_page(browser: Browser, *paths: str) -> Page:
     page = browser.new_page()
-    page.goto('https://rss.tatneft.ru')
+    parsed_paths = '/'.join(paths)
+    page.goto(f'https://rss.tatneft.ru/{parsed_paths}')
     return page
 
 
@@ -22,4 +24,17 @@ def _start_clicking_buttons(page: Page) -> None:
                     in page.query_selector_all('.cufon-canvas')]
     for alt in buttons_alts[2:]:
         button = page.get_by_alt_text(alt)  # type: ignore
-        button.click(delay=CLICKING_DELAY * 1000)
+        button.click(delay=settings.CLICKING_DELAY * 1000)
+    page.close()
+
+
+def _get_gas_stations_xls(browser: Browser) -> None:
+    gas_stations_page = _open_rss_page(browser, 'locator', 'list')
+    _download_gas_stations_xls(gas_stations_page)
+
+
+def _download_gas_stations_xls(gs_page: Page) -> None:
+    with gs_page.expect_download() as download_info:
+        gs_page.locator('.btn').click()
+    download = download_info.value
+    download.save_as(settings.XLS_PATH)
